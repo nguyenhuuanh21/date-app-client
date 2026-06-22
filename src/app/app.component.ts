497d0b98ac8b476923f8a4200aea4b03528d9a07
nguyenhuuanh21 <nguyenhuuanh.user@gmail.com>
2026-06-22 22:40:33 +0700

    initial commit FE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  private http=inject(HttpClient);
+  protected members=signal<any>([]);
+  async ngOnInit() {
+    // this.http.get('https://localhost:7293/api/Members').subscribe({
+    //   next:(res)=>this.members.set(res),
+    //   error:(err)=>console.log(err),
+    //   complete:()=>console.log('completed')
+    // })
+    this.members.set(await this.getMembers());
+  }
+  async getMembers(){
+    try{
+      return await lastValueFrom(this.http.get('https://localhost:7293/api/Members')) ;
+    }catch(err){
+      console.log(err);
+      throw err;
+    }
+  }
 }
+
+
